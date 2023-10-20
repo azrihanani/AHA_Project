a60818bb253d2d2e5a671ea02bcdf8ab4b75e8e3
azrihanani <148434413+azrihanani@users.noreply.github.com>
2023-10-20 13:10:36 +0800

    Update advertising.py

--- advertising.py
+++ advertising.py
@@ -11,9 +11,9 @@ This app predicts the **Number of Sales!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('Number of TV', 17.2, 296.4, 20.4)
-    Radio = st.sidebar.slider('Number of Radio', 10.8, 49.6, 20.4)
-    Newspaper = st.sidebar.slider('Number of Newspaper', 45.1, 114.0, 20.3)
+    Number_of_TV = st.sidebar.slider('Number of TV', 17.2, 296.4, 20.4)
+    Number_of_Radio = st.sidebar.slider('Number of Radio', 10.8, 49.6, 20.4)
+    Number_of_Newspaper = st.sidebar.slider('Number of Newspaper', 45.1, 114.0, 20.3)
     data = {'TV': Number_of_TV,
             'Radio': Number_of_Radio,
             'Newspaper': Number_of_Newspaper}
